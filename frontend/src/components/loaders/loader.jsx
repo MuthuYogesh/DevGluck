fe2a4678fba9894e387824d569497a0ef9b2c0b0
MuthuYogesh <muthuyogeshb@gmail.com>
2025-07-30 16:39:53 +0530

    Loader Component Name issue Resolved

--- frontend/src/components/loaders/loader.jsx
+++ frontend/src/components/loaders/loader.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Loader() {
   return (
     <div className="flex justify-center items-center h-48">
-      <div 
+      <div
         className="w-12 h-12 border-4 border-dashed rounded-full animate-spin border-blue-600"
         style={{ animation: "spin 2s linear infinite" }}
       ></div>
